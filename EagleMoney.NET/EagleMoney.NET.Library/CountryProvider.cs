7b48bb84b12a08c48b38fc826aca09101a65a28b
Ivaylo Botusharov <ivaylo.botusharov@gmail.com>
2021-01-07 00:44:23 +0200

    CountryProvider - added list of initial countries

--- EagleMoney.NET/EagleMoney.NET.Library/CountryProvider.cs
+++ EagleMoney.NET/EagleMoney.NET.Library/CountryProvider.cs
@@ -14,12 +14,12 @@ namespace EagleMoney.NET.Library
                 new Country("American Samoa", "AS", "ASM", "016"),
                 new Country("Andorra", "AD", "AND", "020"),
                 new Country("Angola", "AO", "AGO", "024"),
-                new Country("Anguilla", "AI", "AIA", "660"),
+                new Country("Anguilla",  "AI", "AIA", "660"),
                 new Country("Antarctica", "AQ", "ATA", "010"),
                 new Country("Antigua and Barbuda", "AG", "ATG", "028"),
                 new Country("Argentina", "AR", "ARG", "032"),
                 new Country("Armenia", "AM", "AFG", "004"),
-                new Country("United States of America (the)", "US", "USA", "840")
+                new Country("United States of America (the)", "US", "USA","840")
             };
             
             return countries;
